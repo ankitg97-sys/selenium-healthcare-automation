504859b7787511cdfc43a9662ac0edd3db01fea4
Ankit Gupta <55848337+ankitg97-sys@users.noreply.github.com>
2024-09-15 04:57:36 +0530

    Update LoginPage.java

--- src/main/java/com/example/pages/LoginPage.java
+++ src/main/java/com/example/pages/LoginPage.java
@@ -1,1 +1,39 @@
+package com.example.pages;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class LoginPage {
+
+    private WebDriver driver;
+
+    // Page elements (locators)
+    @FindBy(id = "username")
+    private WebElement usernameField;
+
+    @FindBy(id = "password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    // Page actions (methods)
+    public void enterUsername(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+}
